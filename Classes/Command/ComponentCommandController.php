67e17ab9cd5b00de663dae319bc2b984624dbf33
Wilhelm Behncke <wilhelm.behncke@protonmail.com>
2020-10-11 12:43:20 +0200

    Add basic command documentation for ComponentCommandController

--- Classes/Command/ComponentCommandController.php
+++ Classes/Command/ComponentCommandController.php
@@ -12,7 +12,7 @@ use PackageFactory\AtomicFusion\PresentationObjects\Domain\Component\ComponentGe
 use PackageFactory\AtomicFusion\PresentationObjects\Domain\Value\ValueGenerator;
 
 /**
- * The command controller for component handling
+ * The command controller for kickstarting PresentationObject components
  */
 class ComponentCommandController extends CommandController
 {
@@ -29,8 +29,10 @@ class ComponentCommandController extends CommandController
     protected $valueGenerator;
 
     /**
-     * @param string $name
-     * @param null|string $packageKey
+     * Create a new PresentationObject component and factory
+     *
+     * @param string $name The name of the new component
+     * @param null|string $packageKey Package key of an optional target package, if not set the configured default package or the first available site package will be used
      * @return void
      */
     public function kickStartCommand(string $name, ?string $packageKey = null): void
@@ -39,11 +41,13 @@ class ComponentCommandController extends CommandController
     }
 
     /**
-     * @param string $componentName
-     * @param string $name
-     * @param string $type
-     * @param array|string[] $values
-     * @param null|string $packageKey
+     * Create a new pseudo-enum value object
+     *
+     * @param string $componentName The name of the component the new pseudo-enum belongs to
+     * @param string $name The name of the new pseudo-enum
+     * @param string $type The type of the new pseudo-enum (must be one of: "string", "int")
+     * @param array|string[] $values A comma-separated list of values for the new pseudo-enum
+     * @param null|string $packageKey Package key of an optional target package, if not set the configured default package or the first available site package will be used
      * @return void
      */
     public function kickStartValueCommand(string $componentName, string $name, string $type, array $values = [], ?string $packageKey = null): void

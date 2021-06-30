db52fe8c47c879cec8c26d910a9a414e11bc1eb3
Bernhard Schmitt <schmitt@sitegeist.de>
2021-06-30 07:51:33 +0200

    Fix typo

--- Classes/Fusion/SelfWrapping.php
+++ Classes/Fusion/SelfWrapping.php
@@ -9,7 +9,7 @@ namespace PackageFactory\AtomicFusion\PresentationObjects\Fusion;
  * The trait for self-wrapping presentation components for inline editing
  *
  * The wrapper function should be created by AbstractComponentPresentationObjectFactory::createWrapper()
- * @deprecated since 3.0 Use PackageFactory\AtomicFusion\PresentationObjects\Presentation\Slot\Content for content integration pusposes instead
+ * @deprecated since 3.0 Use PackageFactory\AtomicFusion\PresentationObjects\Presentation\Slot\Content for content integration purposes instead
  */
 trait SelfWrapping
 {
@@ -21,7 +21,7 @@ trait SelfWrapping
     /**
      * @param string $value
      * @return string
-     * @deprecated since 3.0 Use PackageFactory\AtomicFusion\PresentationObjects\Presentation\Slot\Content for content integration pusposes instead
+     * @deprecated since 3.0 Use PackageFactory\AtomicFusion\PresentationObjects\Presentation\Slot\Content for content integration purposes instead
      */
     final public function wrap(string $value): string
     {

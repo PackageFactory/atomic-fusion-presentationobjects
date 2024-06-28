5e58d560cc2f40deba322cdc6edfaefd11de76b3
Bernhard Schmitt <schmitt@sitegeist.de>
2024-06-28 03:35:38 +0200

    Add property value extractor methods for PHPStan level 9 support

--- Classes/Fusion/AbstractComponentPresentationObjectFactory.php
+++ Classes/Fusion/AbstractComponentPresentationObjectFactory.php
@@ -8,6 +8,7 @@ declare(strict_types=1);
 
 namespace PackageFactory\AtomicFusion\PresentationObjects\Fusion;
 
+use Neos\ContentRepository\Core\Projection\ContentGraph\Node;
 use Neos\ContentRepositoryRegistry\ContentRepositoryRegistry;
 use Neos\Eel\ProtectedContextAwareInterface;
 use Neos\Flow\Annotations as Flow;
@@ -28,6 +29,65 @@ abstract class AbstractComponentPresentationObjectFactory implements
 
     #[Flow\Inject]
     protected Translator $translator;
+
+    /**
+     * @template T
+     * @param class-string<T> $expectedType
+     * @return ?T
+     */
+    final protected static function getObjectValue(Node $node, string $propertyName, string $expectedType): mixed
+    {
+        $propertyValue = $node->getProperty($propertyName);
+
+        return $propertyValue instanceof $expectedType
+            ? $propertyValue
+            : null;
+    }
+
+    /**
+     * @template T
+     * @param class-string<T> $expectedType
+     * @return array<int,T>|null
+     */
+    final protected static function getObjectArrayValue(Node $node, string $propertyName, string $expectedType): ?array
+    {
+        $propertyValue = $node->getProperty($propertyName);
+        if (!is_array($propertyValue)) {
+            return null;
+        }
+        return array_filter(
+            $propertyValue,
+            fn (mixed $item): bool => $item instanceof $expectedType
+        );
+    }
+
+    final protected static function getStringValue(Node $node, string $propertyName): ?string
+    {
+        $propertyValue = $node->getProperty($propertyName);
+
+        return is_string($propertyValue) ? $propertyValue : null;
+    }
+
+    final protected static function getBoolValue(Node $node, string $propertyName): ?bool
+    {
+        $propertyValue = $node->getProperty($propertyName);
+
+        return is_bool($propertyValue) ? $propertyValue : null;
+    }
+
+    final protected static function getIntValue(Node $node, string $propertyName): ?int
+    {
+        $propertyValue = $node->getProperty($propertyName);
+
+        return is_int($propertyValue) ? $propertyValue : null;
+    }
+
+    final protected static function getFloatValue(Node $node, string $propertyName): ?float
+    {
+        $propertyValue = $node->getProperty($propertyName);
+
+        return is_float($propertyValue) ? $propertyValue : null;
+    }
 
     /**
      * All methods are considered safe

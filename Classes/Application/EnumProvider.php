130c85b0f6dd3ae88ed1aecdabb1ebe0b925bd97
Bernhard Schmitt <schmitt@sitegeist.de>
2025-05-19 12:37:42 +0200

    Make nullability explicit

--- Classes/Application/EnumProvider.php
+++ Classes/Application/EnumProvider.php
@@ -36,7 +36,7 @@ final class EnumProvider extends AbstractDataSource implements ProtectedContextA
      * @param array<string|int,string> $arguments
      * @return array<string|int,array<string,string>>
      */
-    public function getData(NodeInterface $node = null, array $arguments = []): array
+    public function getData(?NodeInterface $node = null, array $arguments = []): array
     {
         if (!array_key_exists('enumName', $arguments) || !is_string($arguments['enumName'])) {
             throw new \InvalidArgumentException('Argument "enumName" must be provided.', 1625297174);
